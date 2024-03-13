cccfa5f552e2d793ed012ef5cdc2bbe6bf595ef9
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-03-13 22:58:18 +0200

    Update stripe-client.ts

--- apps/web/src/lib/stripe-client.ts
+++ apps/web/src/lib/stripe-client.ts
@@ -1,6 +1,8 @@
 import { loadStripe } from "@stripe/stripe-js";
+import { config } from "dotenv";
 
 export const getStripe = () => {
+  config();
   console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
   return loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 };
