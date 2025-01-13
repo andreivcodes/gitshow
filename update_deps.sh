062af8fdbf1c45b182b5e0b3fa8441db884133e9
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2025-01-13 10:39:10 +0200

    deps

--- update_deps.sh
+++ update_deps.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Update root dependencies
-npx npm-check-updates -u
-yarn install
-
-# Update dependencies in each workspace
-for dir in $(yarn workspaces list --json | jq -r '.location'); do
-  (cd "$dir" && npx npm-check-updates -u && yarn install)
-done
+npx syncpack update
+npx syncpack fix-mismatches
+npx syncpack format
